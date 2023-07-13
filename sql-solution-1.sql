bd5ae387db947c09347f97e8c305959817770d54
severian44 <96204521+severian44@users.noreply.github.com>
2023-07-13 17:55:57 +0300

    initial commit

---
+++ sql-solution-1.sql
@@ -0,0 +1,8 @@
+Create table If Not Exists Person (Id int, Email varchar(255));
+Truncate table Person;
+insert into Person (id, email) values ('1', 'john@example.com');
+insert into Person (id, email) values ('2', 'bob@example.com');
+insert into Person (id, email) values ('3', 'john@example.com');
+
+#Solution for Delete duplicate emails problem using Join:
+DELETE pers1 FROM Person pers1, Person pers2 WHERE (pers1.Email = pers2.Email) AND (pers1.Id > pers2.Id);
